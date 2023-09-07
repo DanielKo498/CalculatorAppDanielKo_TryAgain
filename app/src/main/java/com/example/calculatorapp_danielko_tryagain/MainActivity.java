53bb398f61b96fa76889217fd9540476207f050c
DanielKo498 <ko7672@students.d211.org>
2023-09-07 09:39:04 -0500

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculatorapp_danielko_tryagain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
