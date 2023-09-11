b02078f52bbeaea58072e2ec9f94bf2d79b649e4
DanielKo498 <ko7672@students.d211.org>
2023-09-10 22:45:33 -0500

    Functionality of operations complete
    work on the theme and make it look better overall. New buttons? Add individual numbers? Enhancements

--- app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.calculatorapp_danielko_tryagain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add(View v){
+        EditText input1 = findViewById(R.id.num1);
+        int num1 = Integer.parseInt(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        int num2 = Integer.parseInt(input2.getText().toString());
+        int sum = num1 + num2;
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + sum);
+    }
+
+    public void subtract(View v){
+        EditText input1 = findViewById(R.id.num1);
+        int num1 = Integer.parseInt(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        int num2 = Integer.parseInt(input2.getText().toString());
+        int sum = num1 - num2;
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + sum);
+    }
+
+    public void multiply(View v){
+        EditText input1 = findViewById(R.id.num1);
+        int num1 = Integer.parseInt(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        int num2 = Integer.parseInt(input2.getText().toString());
+        int product = num1*num2;
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + product);
+    }
+
+    public void divide(View v){
+        EditText input1 = findViewById(R.id.num1);
+        int num1 = Integer.parseInt(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        int num2 = Integer.parseInt(input2.getText().toString());
+        int answer = num1/num2;
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + answer);
     }
     // hello
 }
