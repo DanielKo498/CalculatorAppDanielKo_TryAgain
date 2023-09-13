ae0228d219b14cf07ff2764ff3eb8bc2bd4d479d
DanielKo498 <ko7672@students.d211.org>
2023-09-13 08:05:26 -0500

    Changed the while loop to if loop and that fixed it for now

--- app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculatorapp_danielko_tryagain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     public String result = "";
     double input1;
     double input2;
+
+    double num1;
+    double num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void numbers(View v){
         TextView input1 = findViewById(R.id.num1);
-        Double num1 = 0.0;
-        Double num2 = 0.0;
-        while(v.getId() != R.id.addButton || v.getId() != R.id.subtractButton || v.getId() != R.id.divideButton || v.getId() != R.id.multiplyButton){
+        if(v.getId() != R.id.addButton && v.getId() != R.id.subtractButton && v.getId() != R.id.divideButton && v.getId() != R.id.multiplyButton){
+            Log.i("result", "entered while loop");
             if(v.getId() == R.id.button1){
                 result += 1;
                 num1 += 1;
                 input1.setText(result);
+                Log.i("result", num1 + "");
             }
-            if(v.getId() == R.id.button2){
+            else if(v.getId() == R.id.button2){
                 result += 2;
                 num2 += 2;
                 input1.setText(result);
@@ -47,23 +51,23 @@ public class MainActivity extends AppCompatActivity {
             input1.setText(result);
         }
 
-        while(v.getId() != R.id.addButton || v.getId() != R.id.subtractButton || v.getId() != R.id.divideButton || v.getId() != R.id.multiplyButton){
-            if(v.getId() == R.id.button1){
-                result += 1;
-                num2 += 1;
-                input1.setText(result);
-            }
-            if(v.getId() == R.id.button2){
-                result += 2;
-                num2 += 2;
-                input1.setText(result);
-            }
-        }
-        if(v.getId() == R.id.addButton){
-            TextView answerTV = findViewById(R.id.answerTV);
-            String answer = add(num1, num2) + "";
-            answerTV.setText(answer);
-        }
+//        while(v.getId() != R.id.addButton || v.getId() != R.id.subtractButton || v.getId() != R.id.divideButton || v.getId() != R.id.multiplyButton){
+//            if(v.getId() == R.id.button1){
+//                result += 1;
+//                num2 += 1;
+//                input1.setText(result);
+//            }
+//            if(v.getId() == R.id.button2){
+//                result += 2;
+//                num2 += 2;
+//                input1.setText(result);
+//            }
+//        }
+//        if(v.getId() == R.id.addButton){
+//            TextView answerTV = findViewById(R.id.answerTV);
+//            String answer = add(num1, num2) + "";
+//            answerTV.setText(answer);
+//        }
 
 
     }
