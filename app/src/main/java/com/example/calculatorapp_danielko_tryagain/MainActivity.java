caf1d7cb0fdea59e29f6f2cb3b16b7807af39f48
DanielKo498 <ko7672@students.d211.org>
2023-09-13 23:45:03 -0500

    Everything worked; added a clear all button function, added a equation to show operation but won't compile after I added colors and themes

--- app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp_danielko_tryagain/MainActivity.java
@@ -3,82 +3,75 @@ package com.example.calculatorapp_danielko_tryagain;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public String result = "";
-    double input1;
-    double input2;
 
-    double num1;
-    double num2;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        result = "";
     }
 
-    public void switchScreens(View v){
-
-
-    }
-
-    public void numbers(View v){
-        TextView input1 = findViewById(R.id.num1);
-        if(v.getId() != R.id.addButton && v.getId() != R.id.subtractButton && v.getId() != R.id.divideButton && v.getId() != R.id.multiplyButton){
-            Log.i("result", "entered while loop");
-            if(v.getId() == R.id.button1){
-                result += 1;
-                num1 += 1;
-                input1.setText(result);
-                Log.i("result", num1 + "");
-            }
-            else if(v.getId() == R.id.button2){
-                result += 2;
-                num2 += 2;
-                input1.setText(result);
-            }
-        }
-
-        // add operation to result after while loop breaks
-        if(v.getId() == R.id.addButton){
-            result+= "+";
-            input1.setText(result);
-        }
-
-//        while(v.getId() != R.id.addButton || v.getId() != R.id.subtractButton || v.getId() != R.id.divideButton || v.getId() != R.id.multiplyButton){
-//            if(v.getId() == R.id.button1){
-//                result += 1;
-//                num2 += 1;
-//                input1.setText(result);
-//            }
-//            if(v.getId() == R.id.button2){
-//                result += 2;
-//                num2 += 2;
-//                input1.setText(result);
-//            }
-//        }
-//        if(v.getId() == R.id.addButton){
-//            TextView answerTV = findViewById(R.id.answerTV);
-//            String answer = add(num1, num2) + "";
-//            answerTV.setText(answer);
-//        }
-
-
-    }
-
-    public static Double add(Double input1, Double input2){
-        double num1 = (input1);
-        double num2 = (input2);
+    public void add(View v) {
+        EditText input1 = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(input2.getText().toString());
         double sum = num1 + num2;
-        return sum;
+        TextView equationTV = findViewById(R.id.equationTV);
+        equationTV.setText(num1 + " + " + num2 + " =");
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + sum);
+    }
+
+    public void subtract(View v) {
+        EditText input1 = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(input2.getText().toString());
+        double sum = num1 - num2;
+        TextView equationTV = findViewById(R.id.equationTV);
+        equationTV.setText(num1 + " - " + num2 + " =");
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + sum);
+    }
+
+    public void multiply(View v) {
+        EditText input1 = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(input2.getText().toString());
+        double product = num1 * num2;
+        TextView equationTV = findViewById(R.id.equationTV);
+        equationTV.setText(num1 + " X " + num2 + " =");
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + product);
+    }
+
+    public void divide(View v) {
+        EditText input1 = findViewById(R.id.num1);
+        double num1 = Double.parseDouble(input1.getText().toString());
+        EditText input2 = findViewById(R.id.num2);
+        double num2 = Double.parseDouble(input2.getText().toString());
+        double answer = num1 / num2;
+        TextView equationTV = findViewById(R.id.equationTV);
+        equationTV.setText(num1 + " / " + num2 + " =");
+        TextView answerTV = findViewById(R.id.answerTV);
+        answerTV.setText("" + answer);
+    }
+
+    public void clearAll(View v){
+        TextView equationTV = findViewById(R.id.equationTV);
+        TextView answerTV = findViewById(R.id.answerTV);
+        EditText input1 = findViewById(R.id.num1);
+        EditText input2 = findViewById(R.id.num2);
+        equationTV.setText("");
+        answerTV.setText("");
+        input1.setText("");
+        input2.setText("");
 
     }
 
-    
 }
